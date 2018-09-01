63d0bc0aacdba11069860c89ae221aacdf1ed39b
Bryan Juma <jumabryan10@gmail.com>
2018-09-01 13:58:03 +0300

    api secret key working

--- manage.py
+++ manage.py
@@ -1,18 +1,11 @@
-from app import create_app #Import create app function
-from flask_script import Manager,Server #Import manager class to initialize our extension and server class to launch our server
+from app import create_app
+from flask_script import Manager,Server
 
+#creating app instance
 # Creating app instance
-app = create_app('development') #Call create_app function and pass the configuration options
+app = create_app('development')
 
-manager = Manager(app) #Instantiate manager class by passing the app instance
-manager.add_command('server',Server) #Command line on how our app should run
-
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
+manager = Manager(app)
+manager.add_command('server',Server)
 if __name__ == '__main__':
     manager.run()
