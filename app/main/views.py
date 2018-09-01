9755a2a57001ad7fbf889977529c0df26e5b926f
Bryan Juma <jumabryan10@gmail.com>
2018-09-01 12:20:15 +0300

    views py modified

--- app/main/views.py
+++ app/main/views.py
@@ -1,24 +1,21 @@
-from flask import render_template, request, redirect, url_for
+#Files imports
+from flask import render_template,request,redirect,url_for
+# from app import app
 from . import main
-from ..request import get_articles, get_news
+from ..requests import get_source,get_article
+from ..models import Source
 
 
-#views
-@main.route('/')
+#Views
+@main.route("/")
 def index():
+    return render_template("index.html")
+
+@main.route('/articles') #Define route decorator with main blueprint instance
+def articles(): #Article function that
     '''
-    view root page that returns the index page and its data
+    View article page function that returns the articles details page and its data
     '''
-    title = "Home || News Sources"
 
-    all_news = get_news('sports')
-    general_news = get_news('general')
-    tech_news = get_news('technology')
-    bus_news = get_news('business')
-    sci_news = get_news('science')
-
-
-    # print(all_news)
-
-
-    return render_template('index.html', title= title, sports = all_news, general = general_news, technology = tech_news, business = bus_news, science = sci_news)
+    articles = get_article()
+    return render_template('article.html', articles = articles) #Returns response of article.html
