11c8a015b15eace1b4092a8bb2e94862d1001579
Bryan Juma <jumabryan10@gmail.com>
2018-09-04 09:48:35 +0300

    docstring added

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@ export NEWS_API_KEY='df343d6098ba4e2b8e62adfb9038cf90'
 export SECRET_KEY='3F82ab80'
 
 python3.6 manage.py server
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
